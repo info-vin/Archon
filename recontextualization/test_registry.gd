ead6d934956a588c8800484739c6e188b08e1a25
tek Atrust <atrustek999@gmail.com>
2026-07-01 11:08:28 +0800

    chore: remove one-off debug and utility scripts

--- recontextualization/test_registry.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-
-func _init():
-    var registry = load("res://src/managers/CardRegistry.gd").new()
-    registry._ready()
-    print("Cards loaded: ", registry.cards.keys())
-    quit()
